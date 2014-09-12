d5c52b036dcfd92128c662c6a1c22f101cd8604d
Fabio <fab@Fabios-iMac.local>
2014-09-12 11:45:05 +0200

    mock home page and template infrastructure

--- main.py
+++ main.py
@@ -15,10 +15,61 @@
 # limitations under the License.
 #
 import webapp2
+import jinja2
+import os
+import logging
 
-class MainHandler(webapp2.RequestHandler):
+
+
+template_dir=os.path.join(os.path.dirname(__file__), 'templates')
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(template_dir),
+    #extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+
+class BaseRequestHandler(webapp2.RequestHandler):
+    
+
+   
+    
+    
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+
+
+    def render(self, template_name, template_vars={}):
+     values={}
+     values.update(template_vars)
+     try:
+       template=JINJA_ENVIRONMENT.get_template(template_name)
+       self.write(template.render(**values))
+     except:
+       logging.error("Rendering Exception for " + template_name)
+       self.abort(404)
+
+
+
+    def dispatch(self):            
+        webapp2.RequestHandler.dispatch(self)
+
+    # try:
+       # Dispatch the request.
+       #webapp2.RequestHandler.dispatch(self)
+    # finally:
+       # Save all sessions.
+       #self.session_store.save_sessions(self.response)
+
+
+
+
+
+class MainHandler(BaseRequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render('index.html')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
